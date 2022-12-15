d75be2db89c05e0d76fa5d72abe902d8d730cac5
Julien <julien@nozav.org>
2022-12-15 18:30:53 +0100

    Replace range by tuples

--- code/15.py
+++ code/15.py
@@ -13,9 +13,9 @@ def parse(line):
 
 
 def reduce_range(r1, r2):
-    if r1.start > r2.stop or r2.start > r1.stop:
+    if r1[0] > r2[1] or r2[0] > r1[1]:
         return None
-    return range(min(r1.start, r2.start), max(r1.stop, r2.stop))
+    return (min(r1[0], r2[0]), max(r1[1], r2[1]))
 
 
 def reduce_ranges(ranges):
@@ -26,9 +26,8 @@ def reduce_ranges(ranges):
             for j in range(i + 1, len(ranges)):
                 if (res := reduce_range(ranges[i], ranges[j])) is not None:
                     changed = True
-                    del ranges[i]
-                    del ranges[j - 1]
-                    ranges.append(res)
+                    ranges[i] = res
+                    del ranges[j]
                     break
             if changed:
                 break
@@ -38,10 +37,10 @@ def reduce_ranges(ranges):
 def get_no_beacons(measures, y):
     no_beacon = []
     for l in measures:
-        width = int(l["dist"] - abs(y - l["sensor"].imag))
+        width = l["dist"] - abs(y - l["sensor"].imag)
         if width >= 0:
-            r = int(l["sensor"].real)
-            interv = range(r - width, r + width + 1)
+            r = l["sensor"].real
+            interv = (int(r - width), int(r + width + 1))
             no_beacon.append(interv)
     return reduce_ranges(no_beacon)
 
@@ -52,12 +51,12 @@ def get_no_beacons(measures, y):
 def solve1(file, y_line):
     measures = map(parse, open(file, "r").read().split("\n"))
     no_beacon = get_no_beacons(measures, y_line)
-    size = sum(map(lambda r: r.stop - r.start - 1, no_beacon))
+    size = sum(map(lambda r: r[1] - r[0] - 1, no_beacon))
     return size
 
 
-print(solve1(day.test_file, y_line=10))
-print(solve1(day.valid_file, y_line=2000000))
+solve1(day.test_file, y_line=10)
+%timeit solve1(day.valid_file, y_line=2000000)
 
 
 # Second puzzle ---------
@@ -65,8 +64,8 @@ print(solve1(day.valid_file, y_line=2000000))
 
 def solve2(file, limit):
     measures = list(map(parse, open(file, "r").read().split("\n")))
-    x = -1
-    for y in range(limit + 1, 0, -1):
+    x = y = -1
+    for y in range(0, limit + 1):
         if len(no_beacon := get_no_beacons(measures, y)) > 1:
             x = min(no_beacon[0].stop, no_beacon[1].stop)
             break
@@ -75,5 +74,5 @@ def solve2(file, limit):
     return (x, y), x * 4000000 + y
 
 
-print(solve2(day.test_file, limit=20))
-print(solve2(day.valid_file, limit=4000000))  # 11796491041245
+solve2(day.test_file, limit=20)
+solve2(day.valid_file, limit=4000000)  # 11796491041245
