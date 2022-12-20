93a8d5c6fc48a375335018d82748a6dc259841f4
Julien <julien@nozav.org>
2022-12-20 20:05:37 +0100

    Cleanup

--- code/20.py
+++ code/20.py
@@ -6,7 +6,7 @@ day = Day(20)
 
 def solve(orig, n_mix=1):
     n = len(orig)
-    # Transform values into (divider, rest tuples)
+    # Transform values into (divider, rest) tuples
     a = list(
         map(
             lambda x: (x // (n - 1), x % (n - 1))
@@ -17,17 +17,17 @@ def solve(orig, n_mix=1):
     )
     # Keep original positions movement
     a_positions = list(range(len(a)))
-    for mix in range(n_mix):
+    for _ in range(n_mix):
         for i in range(len(a)):
             pos = a_positions.index(i)
-            mul, item = a[pos]
+            mult, item = a[pos]
             new_pos = pos + item
             if new_pos >= n:
                 new_pos = new_pos % (n - 1)
             if new_pos <= -n:
                 new_pos = -(-new_pos % (n - 1))
             del a[pos]
-            a.insert(new_pos, (mul, item))
+            a.insert(new_pos, (mult, item))
             a_positions.remove(i)
             a_positions.insert(new_pos, i)
     zero_pos = a.index((0, 0))
